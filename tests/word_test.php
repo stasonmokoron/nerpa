1b7ca1f83fd8434135886b043b070f0b9d91e698
stasonmokoron <tkachkras@gmail.com>
2015-12-21 04:11:15 +0300

    обновлены тесты

--- tests/word_test.php
+++ tests/word_test.php
@@ -3,63 +3,57 @@
 	require_once 'c:/server/data/htdocs/meaning.inc.php';
 	class WordTest extends PHPUnit_Framework_TestCase{
 	
-		public function testCountStringOccurrencesInArray(){
-			$test = new Word();
+		public function testGetMeaningsArrayWithLemma(){
 			
-			$arrObj[0]=new Word();
-			$arrObg[0]->arr_me[0]=new Meaning();
-			$arrObg[0]->arr_me[0]->arr_senseLemmatize("способности живого существа воспринимать внешние впечатления");
+			$test = new Word();
+			$test->me[0]="способности живого существа воспринимать внешние впечатления";
+			$test->arr_meaning[0]=new Meaning();
+			$test->arr_meaning[0]->setArrLemmasLemmatizeText($test->me[0]);
 			
 			//метод assertEquals(), который первым обязательным параметром принимает ожидаемое значение, вторым актуальное и проверяет их соответствие.
-			$this->assertEquals(array(1), $test->countStringOccurrencesInArray("способность", $arrObg[0]->arr_me));	
+			$this->assertEquals(array("способности живого существа воспринимать внешние впечатления"), $test->getMeaningsArrayWithLemma("способность"));	
 		}
 		
-		public function testCountStringOccurrencesInArray2(){
+		public function testGetMeaningsArrayWithLemma2(){
 			$test = new Word();
-			
-			$arrObj[0]=new Word();			
-			$arrObg[0]->arr_me[0]=new Meaning();
-			$arrObg[0]->arr_me[0]->arr_senseLemmatize("способности живого существа воспринимать внешние впечатления");
-			$arrObg[0]->arr_me[1]=new Meaning();
-			$arrObg[0]->arr_me[1]->arr_senseLemmatize("живого существа воспринимать внешние впечатления");
-			$arrObg[0]->arr_me[2]=new Meaning();
-			$arrObg[0]->arr_me[2]->arr_senseLemmatize("живого существа воспринимать, СпосОбностей, внешние впечатления");
+			$test -> me = array ("способности живого существа воспринимать внешние впечатления",
+							"живого существа воспринимать внешние впечатления",
+							"живого существа воспринимать способностей внешние впечатления");
+			for ($i = 0; $i < 3; $i++) {
+				$test->arr_meaning[$i] = new Meaning;
+				$test->arr_meaning[$i]->setArrLemmasLemmatizeText($test->me[$i]);
+			}
 			
 			//метод assertEquals(), который первым обязательным параметром принимает ожидаемое значение, вторым актуальное и проверяет их соответствие.
-			$this->assertEquals(array(1,3), $test->countStringOccurrencesInArray("способность", $arrObg[0]->arr_me));	
+			$this->assertEquals(array("способности живого существа воспринимать внешние впечатления",
+									"живого существа воспринимать способностей внешние впечатления"), $test->getMeaningsArrayWithLemma("способность"));	
 		}
 		
-		public function testCountStringOccurrencesInArrayNull(){
+		public function testGetMeaningsArrayWithLemmaNull(){
 			$test = new Word();
-			
-			$arrObj[0]=new Word();
-			$arrObg[0]->arr_me[0]=new Meaning();
-			$arrObg[0]->arr_me[0]->arr_senseLemmatize("способность живого существа воспринимать внешние впечатления");
+			$test->arr_meaning[0]=new Meaning();
+			$test->arr_meaning[0]->setArrLemmasLemmatizeText("способность живого существа воспринимать внешние впечатления");
 			
 			//метод assertEquals(), который первым обязательным параметром принимает ожидаемое значение, вторым актуальное и проверяет их соответствие.
-			$this->assertEquals([], $test->countStringOccurrencesInArray("", $arrObg[0]->arr_me));	
+			$this->assertEquals([], $test->getMeaningsArrayWithLemma(""));	
 		}
 		
-		public function testCountStringOccurrencesInArrayNull2(){
+		public function testGetMeaningsArrayWithLemmaNull2(){
 			$test = new Word();
-			
-			$arrObj[0]=new Word();
-			$arrObg[0]->arr_me[0]=new Meaning();
-			$arrObg[0]->arr_me[0]->arr_senseLemmatize("живого существа воспринимать внешние впечатления");
+			$test->arr_meaning[0]=new Meaning();
+			$test->arr_meaning[0]->setArrLemmasLemmatizeText("живого существа воспринимать внешние впечатления");
 			
 			//метод assertEquals(), который первым обязательным параметром принимает ожидаемое значение, вторым актуальное и проверяет их соответствие.
-			$this->assertEquals([], $test->countStringOccurrencesInArray("способность", $arrObg[0]->arr_me));	
+			$this->assertEquals([], $test->getMeaningsArrayWithLemma("способность"));	
 		}
 		
-		public function testCountStringOccurrencesInArrayNull3(){
+		public function testGetMeaningsArrayWithLemmaNull3(){
 			$test = new Word();
-			
-			$arrObj[0]=new Word();
-			$arrObg[0]->arr_me[0]=new Meaning();
-			$arrObg[0]->arr_me[0]->arr_senseLemmatize("");
+			$test->arr_meaning[0]=new Meaning();
+			$test->arr_meaning[0]->setArrLemmasLemmatizeText("");
 			
 			//метод assertEquals(), который первым обязательным параметром принимает ожидаемое значение, вторым актуальное и проверяет их соответствие.
-			$this->assertEquals([], $test->countStringOccurrencesInArray("способность", $arrObg[0]->arr_me));	
+			$this->assertEquals([], $test->getMeaningsArrayWithLemma("способность"));	
 		}
 		
 	}	
